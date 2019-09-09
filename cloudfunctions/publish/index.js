7a85e25242f2d97dc590bd7c1b17377baa5121e0
LuckyJQ <shenjianqi1111@126.com>
2019-09-09 16:24:21 +0800

    补充云开发接口

---
+++ cloudfunctions/publish/index.js
@@ -0,0 +1,17 @@
+const cloud = require('wx-server-sdk')
+cloud.init()
+const db = cloud.database()
+
+exports.main = async (event, context) => {
+  try {
+    return await db.collection('publish_collection').add({
+      data: {
+        ...event,
+        publish_time: Date.now(),
+        update_time: Date.now()
+      }
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
